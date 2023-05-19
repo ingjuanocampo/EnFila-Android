61708d461466de8cfc29bcabae149dada575b968
juan ocampo <ing.juanocampo@gmail.com>
2023-05-18 21:04:27 -0500

    Login toolbar

--- app/app/src/main/java/com/ingjuanocampo/enfila/android/login/ActivityLogin.kt
+++ app/app/src/main/java/com/ingjuanocampo/enfila/android/login/ActivityLogin.kt
@@ -3,7 +3,12 @@ package com.ingjuanocampo.enfila.android.login
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.lifecycle.Observer
+import androidx.navigation.NavController
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.ingjuanocampo.enfila.android.R
 import com.ingjuanocampo.enfila.android.login.viewmodel.ViewModelLogin
 import dagger.hilt.android.AndroidEntryPoint
@@ -13,6 +18,8 @@ class ActivityLogin : AppCompatActivity() {
 
     val viewModel: ViewModelLogin by viewModels()
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewModel.state.observe(
@@ -21,5 +28,10 @@ class ActivityLogin : AppCompatActivity() {
             },
         )
         setContentView(R.layout.activity_login)
+
+        navController = findNavController(this, R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        findViewById<Toolbar>(R.id.toolbarWidget)
+            .setupWithNavController(navController, appBarConfiguration)
     }
 }
